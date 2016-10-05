927e0d7ffe3b3eabe329d3729b6c55b93c960f3b
Gioh Kim <gurugio@hanmail.net>
2016-10-05 22:51:55 +0200

    dining philosophers: finish

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,20 +1,66 @@
+use std::thread;
+use std::sync::{Mutex, Arc};
+
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+                                 
+        println!("{} is eating.", self.name);
+        thread::sleep_ms(1000);
+        println!("{} is done eating.", self.name);
     }
 }
 
 
+
 fn main() {
-    let p1 = Philosopher::new("butler");
-    let p2 = Philosopher::new("p2");
-    let p3 = Philosopher::new("p3");
-    let p4 = Philosopher::new("p4");
-    let p5 = Philosopher::new("p5");
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new( () ),
+        Mutex::new( () ),
+        Mutex::new( () ),
+        Mutex::new( () ),
+        Mutex::new( () ),
+    ]});
+        
+    
+    let philosophers = vec![
+        Philosopher::new("butler", 0, 1),
+        Philosopher::new("p2", 1, 2),
+        Philosopher::new("p3", 2, 3),
+        Philosopher::new("p4", 3, 4),
+        Philosopher::new("p5", 0, 4),
+    ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+                                     
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
 }
